cde5e39e21f4c7b21372c82edd7ac2b01fcf704d
asenka <91136719+assenka@users.noreply.github.com>
2022-12-26 23:10:03 +0300

    edit hw3

--- programming-2022-2023/First Semester/Homework-3_2022.10.27/main.cpp
+++ programming-2022-2023/First Semester/Homework-3_2022.10.27/main.cpp
@@ -1,20 +1,21 @@
-#include "kvaternion.cpp"
+#include "kvaternion.hpp"
 using namespace std;
 
 int main(){
     Kvaternion k1 (0, 0, 0, 0);
-    cout << k1 << endl;
+    cout << "k1 = " << k1 << endl;
     k1.set(1, 2, 3, 4);
-    cout << k1 << endl;
+    cout << "k1 = " << k1 << endl;
     Kvaternion k2 (0, 2, 0, 2);
-    cout << k1 + k2 << endl;
-    cout << k2.getIm_z() << endl;
+    cout << "k1 + k2 = " << k1 + k2 << endl;
+    cout << "im_z_k2 = " << k2.getIm_z() << endl;
     k1 += k2;
-    cout << k1 << endl;
+    cout << "k1 = " << k1 << endl;
     k1.setRe(8);
-    cout << k1 << endl;
-    cout << k2.abs() << endl;
-    cout << k1.conjugate() << endl;
+    cout << "k1 = " << k1 << endl;
+    cout << "|k2| = " << k2.abs() << endl;
+    cout << "conjugated k = " << k1.conjugate() << endl;
 
     return 0;
 }
+
